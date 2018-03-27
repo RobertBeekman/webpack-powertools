4013ff7386cd9ef0a319a91b98b4c41fe10e2049
Jonathan Mezach <jonathanm@infosupport.com>
2018-03-27 10:26:43 +0200

    Remove logging statement from build file

--- dist/NormalizedModulesPlugin.js
+++ dist/NormalizedModulesPlugin.js
@@ -2,8 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class NormalizedModulesPlugin {
     apply(normalModuleFactory) {
+        // Add a "module" plugin that normalizes requests to node_modules so that they are always relative from the current app
+        // This avoids the issue of modules being included in multiple bundles while they should be delegated from the vendor bundle
         normalModuleFactory.plugin("module", (module) => {
-            console.log("NormalizedModulesPlugin", module.userRequest);
             if (module.userRequest && module.userRequest.indexOf("node_modules") > 0) {
                 module.userRequest = "./" + module.userRequest.substring(module.userRequest.indexOf("node_modules"));
             }
