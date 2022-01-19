8fd89ad4b92f94c9419db6eddc7e39c2b409bc2b
Robert Beekman <robert.beekman@rr-wfm.com>
2022-01-19 14:37:27 +0100

    Fix indents

--- src/AlwaysIncludeHmrBootstrapPlugin.ts
+++ src/AlwaysIncludeHmrBootstrapPlugin.ts
@@ -9,26 +9,26 @@ export class AlwaysIncludeHmrBootstrapPlugin {
     }
 
     apply(compiler: Webpack.Compiler) {
-      if (!compiler.options.entry) {
-        // If there is no entry, simply do nothing
-        return;
-      }
+        if (!compiler.options.entry) {
+            // If there is no entry, simply do nothing
+            return;
+        }
 
-      if (!compiler.options.devServer) {
-        // If the devServer is not configured, simply do nothing
-        return;
-      }
+        if (!compiler.options.devServer) {
+            // If the devServer is not configured, simply do nothing
+            return;
+        }
 
-      const entry: string[] = compiler.options.entry[this.entryName];
-      if (!entry) {
-        // Couldn't find an entry with the provided name
-        throw new Error(`Unable to find entry with name ${this.entryName}`);
-      }
+        const entry: string[] = compiler.options.entry[this.entryName];
+        if (!entry) {
+            // Couldn't find an entry with the provided name
+            throw new Error(`Unable to find entry with name ${this.entryName}`);
+        }
 
-      const devServerPath = `http://localhost:${compiler.options.devServer.port}${compiler.options.devServer.publicPath}`;
-      compiler.options.entry["app"].splice(0, 0, 
-          "./node_modules/webpack-dev-server/client/index.js?" + devServerPath,
-          "webpack/hot/dev-server"
-      );
+        const devServerPath = `http://localhost:${compiler.options.devServer.port}${compiler.options.devServer.publicPath}`;
+        compiler.options.entry["app"].splice(0, 0,
+            "./node_modules/webpack-dev-server/client/index.js?" + devServerPath,
+            "webpack/hot/dev-server"
+        );
     }
 }
--- src/NamedDelegatedModulesPlugin.ts
+++ src/NamedDelegatedModulesPlugin.ts
@@ -5,26 +5,26 @@ import * as Webpack from "webpack";
  */
 export class NamedDelegatedModulesPlugin {
 
-  apply(compiler: Webpack.Compiler) {
-    compiler.hooks.compile.tap('named-delegated-modules', params => {
-      params.normalModuleFactory.hooks.module.tap('named-delegated-modules', (module) => {
-        const normalModule = module as Webpack.NormalModule;
-        if (normalModule && normalModule.userRequest && normalModule.userRequest.indexOf("node_modules") > 0) {
-          normalModule.userRequest = "./" + normalModule.userRequest.substring(normalModule.userRequest.indexOf("node_modules"));
-        }
-        return module;
-      });
-    })
+    apply(compiler: Webpack.Compiler) {
+        compiler.hooks.compile.tap('named-delegated-modules', params => {
+            params.normalModuleFactory.hooks.module.tap('named-delegated-modules', (module) => {
+                const normalModule = module as Webpack.NormalModule;
+                if (normalModule && normalModule.userRequest && normalModule.userRequest.indexOf("node_modules") > 0) {
+                    normalModule.userRequest = "./" + normalModule.userRequest.substring(normalModule.userRequest.indexOf("node_modules"));
+                }
+                return module;
+            });
+        })
 
-    compiler.hooks.compilation.tap('named-delegated-modules', compilation => {
-      compilation.hooks.beforeModuleIds.tap('named-delegated-modules', modules => {
-        for (const module of modules) {
-          const normalModule = module as Webpack.NormalModule;
-          if (normalModule && normalModule.constructor.name === "DelegatedModule") {
-            normalModule.id = normalModule.request;
-          }
-        }
-      })
-    })
-  }
+        compiler.hooks.compilation.tap('named-delegated-modules', compilation => {
+            compilation.hooks.beforeModuleIds.tap('named-delegated-modules', modules => {
+                for (const module of modules) {
+                    const normalModule = module as Webpack.NormalModule;
+                    if (normalModule && normalModule.constructor.name === "DelegatedModule") {
+                        normalModule.id = normalModule.request;
+                    }
+                }
+            })
+        })
+    }
 }
